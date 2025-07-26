80d775a50a908e733c599a11af00d1049df865f9
shahipeasant <005kaziahnaf@gmail.com>
2025-07-26 20:54:10 +0600

    added

--- offline-1/20230204038_offline1.cpp
+++ offline-1/20230204038_offline1.cpp
@@ -1,28 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void sort(vector<int> v, int n){
+void sort(vector<int> &v, int n){
     for(int i=1; i<n; i++){
-        for(int j=i-1; j>=0; j--){
-            if(v[j]<v[i]){
-                swap(v[j+1], v[i]);
-            }
+        int temp = v[i];
+        int j = i-1;
+        while(v[j]>temp&&j>=0){
+            v[j+1]=v[j];
+            j--;
         }
+        v[j+1] = temp;
     }
 }
 
-void Bseacrh(vector<int> v, int n, int key){
+void Bseacrh(vector<int> &v, int n, int key){
+    int flag = 0;
+    int m = 0;
     int lo=0;
     int hi=n-1;
     while(lo<hi){
         int mid=(lo+hi)/2;
-        if(mid==key){
-            hi=mid-1;
+        if(v[mid]==key){
+            flag = 1;
+            m = mid;
+            break;
         }
-        if(mid<key){
+        if(v[mid]<key){
             lo=mid+1;
         }
-        if(mid>key){
+        if(v[mid]>key){
             hi=mid-1;
         }
     }
@@ -31,29 +37,27 @@ void Bseacrh(vector<int> v, int n, int key){
         cout << v[i] << " ";
     }
     cout << endl;
-    if(lo==key){
-        cout << "value " << key << " found at position " << lo-1 << endl;
+    if(flag == 1){
+        cout << "value " << key << " found at position " << m << endl;
     }
     else{
-        cout << "value " << key << " can be inserted at position " << lo-2 << " to maintain sorted order"<< endl;
+        cout << "value " << key << " can be inserted at position " << lo+1 << " to maintain sorted order"<< endl;
     }
 }
 
 int main(){
-    int n;
-    cout << "Enter length of vector: ";
-    cin >> n;
-    vector<int> v(n);
+    vector<int> v;
     cout << "Enter the vector: ";
-    
-    for(int i=0; i<n; i++){
-        cin >> v[i];
+    int t = 0;
+    while(t!=EOF){
+        cin >> t;
+        v.push_back(t);
     }
     
     int key;
     cout << "Enter the value to search: ";
     cin >> key;
-    sort(v, n);
-    Bseacrh(v, n, key);
+    sort(v, v.size());
+    Bseacrh(v, v.size(), key);
     return 0;
 }
