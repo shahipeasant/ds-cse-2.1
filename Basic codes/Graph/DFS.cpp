e0771f0922a257b7bf6c6fad8ce6d722fc100079
shahipeasant <005kaziahnaf@gmail.com>
2025-09-21 04:39:23 +0600

    make every parameter of dfs local

--- Basic codes/Graph/DFS.cpp
+++ Basic codes/Graph/DFS.cpp
@@ -1,50 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> adj;
-vector<int> color;
-vector<int> order;
-int cycleCount = 0;
-
-void dfs(int u) {
-    color[u] = 1;
-    order.push_back(u);
+// DFS function: color, order, and cycleCount are passed by reference
+void dfs(int u, const vector<vector<int>> &adj, vector<int> &color,
+         vector<int> &order, int &cycleCount) 
+{
+    color[u] = 1;       // visiting
+    order.push_back(u);  // record visit order
 
     for (int v : adj[u]) {
         if (color[v] == 0) {
-            dfs(v);
-        }
-        else if(color[v] == 1 && color[u] == 1){
+            dfs(v, adj, color, order, cycleCount);
+        } 
+        else if (color[v] == 1) {
+            // found a back edge -> cycle detected
             cycleCount++;
         }
     }
 
-    color[u] = 2;
+    color[u] = 2; // finished
 }
 
 int main() {
     int n, m;
     cin >> n >> m;
 
-    adj.assign(n + 1, {});
-    color.assign(n + 1, 0);
-
+    vector<vector<int>> adj(n + 1);
     for (int i = 0; i < m; i++) {
         int u, v;
         cin >> u >> v;
         adj[u].push_back(v);
     }
 
+    vector<int> color(n + 1, 0);
+    vector<int> order;
+    int cycleCount = 0;
+
     for (int i = 1; i <= n; i++) {
         if (color[i] == 0) {
-            dfs(i);
+            dfs(i, adj, color, order, cycleCount);
         }
     }
 
     cout << "DFS Visit Order: ";
     for (int u : order) cout << u << " ";
-    cout << "\n";
-    cout << "Number of cycles: " << cycleCount;
+    cout << "\nNumber of cycles (back edges): " << cycleCount << "\n";
 
     return 0;
 }
